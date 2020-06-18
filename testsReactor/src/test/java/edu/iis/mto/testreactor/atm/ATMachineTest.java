082f870699822c8a700d51539de16541e1a27f43
Tomasz.Kucharczyk <215786@edu.p.lodz.pl>
2020-06-18 17:19:33 +0200

    first mocks added

--- testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -2,18 +2,33 @@ package edu.iis.mto.testreactor.atm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Currency;
 
 public class ATMachineTest {
 
+    private Bank bank = Mockito.mock(Bank.class);
+    private Currency currency = Mockito.mock(Currency.class);
+
+    private ATMachine atMachine;
+
     @BeforeEach
-    public void setUp() throws Exception {}
+    public void setUp() throws Exception {
+        atMachine = new ATMachine(bank, currency);
+
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
     }
+
+    
 
 }
